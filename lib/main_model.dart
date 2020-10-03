a00d24f3de41cc85a8197a96421110bda81333c6
shusuke-463 <shusuke463@gmail.com>
2020-10-04 02:00:30 +0900

    third-commit

---
+++ lib/main_model.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MainModel extends ChangeNotifier {
+  String mainText = '新規追加';
+
+  void changeMainText() {
+    mainText = '新規追加';
+    notifyListeners();
+  }
+}
